5ea16710aa7f3da5a65d87abaa08dea6c9066a25
k-kulich <m1ss.darling@yandex.ru>
2023-02-26 14:45:57 +0300

    last change (i hope)

--- button_main.py
+++ button_main.py
@@ -2,19 +2,18 @@ import sys
 import pygame
 from button_d import Button
 from data_loader import load_image
+from constants import SIZE
 
 
 def show_menu():
-    SIZE = 800, 600
-
     screen = pygame.display.set_mode(SIZE)
     pygame.display.set_caption("Main Menu")
 
     resume_b = load_image('resume_b.png')
     quit_b = load_image('exit_b.png')
 
-    resume_button = Button(220, 230, resume_b, 1)
-    quit_button = Button(470, 230, quit_b, 1)
+    resume_button = Button(176, 245, resume_b, 1)
+    quit_button = Button(376, 245, quit_b, 1)
 
     run = True
     while run:
@@ -22,7 +21,7 @@ def show_menu():
         if resume_button.draw(screen):
             return True, False
         if quit_button.draw(screen):
-            exit()
+            return False, False
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
